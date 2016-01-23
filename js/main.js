bbd8a07a19dc443fd108a6d245c9c98136d83465
edwinchoate <edwin.choate@gmail.com>
2016-01-23 15:57:40 -0500

    completes navbar breakpoint functionality (font-size, padding, etc)

--- js/main.js
+++ js/main.js
@@ -1,21 +1,34 @@
 
-var navbarElements = $(".menu-links, .overlay");
-navbarElements.hide();
-var navbarExpanded = false;
+// navbar and menu variables
+var menuBar, navbarElements, navbarExpanded;
+menuBar = $(".menu-bar");
+navbarElements = $(".menu-links, .overlay");
+navbarExpanded = false;
 
-$(".menu-bar").on("click", function() {
+// hide the navbar menu on page load
+navbarElements.hide();
+
+// toggles navbar button's Font Awesome icons and expands/collapses menu
+var menuHandler = function() {
   var _this = $(this);
+  // font-awesome icon
   var icon = _this.find("i");
 
+  // collapsing navbar
   if (navbarExpanded) {
     navbarElements.hide();
     icon.removeClass("fa-close");
     icon.addClass("fa-bars");
     navbarExpanded = false;
-  } else {
+  }
+  // expanding navbar
+  else {
     navbarElements.show();
     icon.removeClass("fa-bars");
     icon.addClass("fa-close");
     navbarExpanded = true;
   }
-});
+}
+
+// add click handler to menu bar button
+menuBar.on("click", menuHandler);
