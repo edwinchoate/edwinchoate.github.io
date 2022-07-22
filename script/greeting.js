a595b2b468f5d4b06f68b7ed2af42ef1758588e5
Edwin Choate <edwin.choate@pm.me>
2022-07-22 13:00:08 -0400

    updates greeting.js with better error handling

--- script/greeting.js
+++ script/greeting.js
@@ -1,17 +1,28 @@
 "use strict";
 /**
 * @author Edwin Choate
-* @version 1.0.0
+* @version 1.0.1
 * @description A simple inline script that greets the user based on time of day
 */
 
 (function(){
-    let hour = new Date().getHours();
+    
+    const MORNING = 'morning';
+    const AFTERNOON = 'afternoon';
+    const EVENING = 'evening';
+
+    const hour = new Date().getHours();
     let timeOfDay = '';
 
-    if (hour >= 17 || hour < 3) timeOfDay = 'evening';  // 5pm to 3am
-    else if (hour < 12) timeOfDay = 'morning';          // 3am to noon
-    else timeOfDay = 'afternoon';                       // noon to 5pm
+    if (hour >= 17 || hour < 3) 
+        timeOfDay = EVENING; // 5pm to 3am
+    else if (hour < 12)
+        timeOfDay = MORNING; // 3am to noon
+    else
+        timeOfDay = AFTERNOON; // noon to 5pm
 
-    if (timeOfDay) document.write('Good ' + timeOfDay + '! ');
+    if ([MORNING, AFTERNOON, EVENING].includes(timeOfDay))
+        document.write('Good ' + timeOfDay + '!');
+    else console.error(`There was a problem with greeting.js\ntimeOfDay: ${timeOfDay}`);
+
 })();
